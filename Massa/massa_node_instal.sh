0ff774ede514818f559faf2ee65b86dec49b1d8b
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2022-06-04 18:58:18 +0700

    Update massa_node_instal.sh

--- Massa/massa_node_instal.sh
+++ Massa/massa_node_instal.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-apt install wget jq pkg-config curl git build-essential libssl-dev -y && wget https://github.com/massalabs/massa/releases/download/TEST.10.1/massa_TEST.10.1_release_linux.tar.gz && tar -xvf massa_TEST.10.1_release_linux.tar.gz && chmod +x ~/massa/massa-node ~/massa/massa-client && printf "[Unit]
+apt install wget jq pkg-config curl git build-essential libssl-dev -y
+
+vers=`wget -qO- https://api.github.com/repos/massalabs/massa/releases/latest | jq -r ".tag_name"`
+
+wget https://github.com/massalabs/massa/releases/download/$(vers)/massa_$(vers)_release_linux.tar.gz && tar -xvf massa_$(vers)_release_linux.tar.gz && chmod +x ~/massa/massa-node ~/massa/massa-client && printf "[Unit]
 Description=Massa Node
 After=network-online.target
 
 [Service]
-User=$USER
+User=root
 WorkingDirectory=$HOME/massa/massa-node
 ExecStart=$HOME/massa/massa-node/massa-node
 Restart=on-failure
