ee5093767014a002186acc019260ba47b849926c
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2022-11-04 14:21:20 +0700

    Update my_instrument.sh

--- my_instrument.sh
+++ my_instrument.sh
@@ -1,1 +1,1 @@
-apt install wget jq curl git htop glances -y && git clone https://github.com/notaprogrammer2019/duf.git /root/duf && cd duf && go build && mv /root/duf/duf /usr/local/bin && rm -rf /root/duf
+apt install wget jq curl ufw git htop glances -y && git clone https://github.com/notaprogrammer2019/duf.git /root/duf && cd duf && go build && mv /root/duf/duf /usr/local/bin && rm -rf /root/duf
