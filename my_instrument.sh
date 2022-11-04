992ab72ddc06d7e5e71dcbef867ed7f3c1ab6704
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2022-11-04 14:21:09 +0700

    Create my_instrument.sh

---
+++ my_instrument.sh
@@ -0,0 +1,1 @@
+apt install wget jq curl git htop glances -y && git clone https://github.com/notaprogrammer2019/duf.git /root/duf && cd duf && go build && mv /root/duf/duf /usr/local/bin && rm -rf /root/duf
