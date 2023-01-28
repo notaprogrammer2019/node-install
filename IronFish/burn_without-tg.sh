54648519f8a3355962789b2906396a759cedd6c0
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2023-01-28 21:36:17 +0700

    Delete IronFish directory

--- IronFish/burn_without-tg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -i node ironfish wallet:burn --amount=5 --assetId=*** --fee=0.00000001 --confirm
--- IronFish/send_without-tg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -i node ironfish wallet:send --amount 5 --assetId=*** --fee 0.00000001 --to dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca --confirm
