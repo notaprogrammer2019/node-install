4208f6c426d78a54e216f8414a174581d13ddeec
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2023-01-28 21:53:40 +0700

    Update auto_mint-burn-send.sh

--- Massa/ironfish/auto_mint-burn-send.sh
+++ Massa/ironfish/auto_mint-burn-send.sh
@@ -68,4 +68,4 @@ docker exec -i node ironfish wallet:mint --metadata="***" --name=*** --amount=10
 printf "#!/bin/bash
 docker exec -i node ironfish wallet:send --amount 5 --assetId=*** --fee 0.00000001 --to dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca --confirm" > /root/send.sh;
 
-chmod +x /root/{burn,mint,senf}.sh;
+chmod +x /root/{burn,mint,send}.sh;
